f27c02a530ffaf5466d0910b0883ce209609546e
Theodor Moroianu <theodor.moroianu@gmail.com>
2024-07-15 15:02:30 +0200

    Update sleep to fix issues at startup.

--- dockerfiles/tidb-tikv-entrypoint.sh
+++ dockerfiles/tidb-tikv-entrypoint.sh
@@ -3,11 +3,13 @@
 
 # Start PD server
 pd-server --name=pd --data-dir=/tmp/pd/data --client-urls="http://0.0.0.0:2379" --peer-urls="http://0.0.0.0:2380" &
-# sleep 10
+
+sleep 3
 
 # Start TiKV server
 tikv-server --pd="127.0.0.1:2379" --data-dir=/tmp/tikv/data &
-# sleep 10
+
+sleep 3
 
 # Start TiDB server
 tidb-server --store=tikv --path="127.0.0.1:2379"
